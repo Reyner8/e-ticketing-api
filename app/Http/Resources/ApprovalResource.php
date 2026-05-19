b5f83e872dd7df7213ba4d10f6855aa3b81d6d5f
Aditya Suryandaru <suryandarua@gmail.com>
2026-05-19 20:31:15 +0800

    feat: create approval resource

---
+++ app/Http/Resources/ApprovalResource.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Enums\ApprovalStatus;
+
+class ApprovalResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'approval' => [
+                'status' => $this->approval_status instanceof ApprovalStatus
+                ? $this->approval_status->value
+                : $this->approval_status,
+
+                'label' => $this->approval_status instanceof ApprovalStatus
+                ? $this->approval_status->label()
+                : $this->approval_status,
+
+                'processed_by' => $this->approver ? [
+                    'id' => $this->approver->id,
+                    'name' => $this->approver->name,
+                    'username' => $this->approver->username,
+                ]: null,
+
+                'processed_at' => $this->approval_date->format('Y-m-d H:i:s'),
+                'rejection_reason' => $this->rejection_reason,
+            ],
+        ];
+    }
+}
