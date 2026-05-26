352047422ae4d9a6d1d7eb970bed115b6baeec5e
Aditya Suryandaru <suryandarua@gmail.com>
2026-05-26 20:40:41 +0800

    feat: update assignment service
    add a function to fill the assignment date automatically after assignment

--- app/Services/Ticket/AssignmentService.php
+++ app/Services/Ticket/AssignmentService.php
@@ -8,10 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Enums\AssignedTeam;
+use App\Enums\ErrorReportStatus;
+use App\Enums\FeatureRequestStatus;
 use App\Enums\TicketStatus;
+use App\Models\ErrorReport;
+use App\Models\FeatureRequest;
+use App\Models\Ticket;
 use App\Models\User;
 use Illuminate\Support\Carbon;
 use Illuminate\Validation\ValidationException;
+use BackedEnum;
 
 class AssignmentService
 {
@@ -48,15 +54,16 @@ class AssignmentService
             $description = $previousAssignee
                 ? "Reassign from '{$previousAssignee}' to '{$user->name}'."
                 : "Assign to '{$user->name}'.";
-            
-                $this->logService->log(
+
+            $this->logService->log(
                 loggable: $resource,
                 action: ActivityAction::Assigned,
                 description: $description,
                 performedBy: Auth::id(),
                 details: array_filter([
                     'assign_to' => $user->name,
-                    'previous_assignee' => $previousAssignee
+                    'previous_assignee' => $previousAssignee,
+                    'assignment_date' => Carbon::now()->format('Y-m-d H:i:s'),
                 ])
             );
 
@@ -75,7 +82,10 @@ class AssignmentService
 
     public function assignToTeam(Model $resource, string $team): Model
     {
+        $this->guardNotAssignable($resource);
+
         $assignedTeam = AssignedTeam::from($team);
+
         $currentTeam = $resource->assigned_team instanceof AssignedTeam
             ? $resource->assigned_team->value
             : $resource->assigned_team;
@@ -83,29 +93,29 @@ class AssignmentService
         if ($currentTeam === $assignedTeam->value) {
             throw ValidationException::withMessages([
                 'team' => [
-                    "Resource is already assigned to team '{$assignedTeam->label()}'"
+                    "Resource is already assigned to '{$assignedTeam->label()}'"
                 ]
             ]);
         }
 
-        $this->guardNotAssignable($resource);
 
-        $assignedTeam = AssignedTeam::from($team);
-        $previousTeam = $resource->assigned_team instanceof AssignedTeam
-            ? $resource->assigned_team->value
+        $previousTeamLabel = $resource->assigned_team instanceof AssignedTeam
+            ? $resource->assigned_team->label()
             : $resource->assigned_team;
-        $previousStatus = $resource->status instanceof \BackedEnum
+
+        $previousStatus = $resource->status instanceof BackedEnum
             ? $resource->status->value
             : $resource->status;
 
-        DB::transaction(function () use ($resource, $assignedTeam, $previousTeam, $previousStatus) {
+        DB::transaction(function () use ($resource, $assignedTeam, $previousTeamLabel, $previousStatus) {
             $resource->update([
                 'assigned_team' => $assignedTeam->value,
+                'assignment_date' => Carbon::now(),
                 'status' => $this->resolveStatusAfterAssignment($resource)
             ]);
 
-            $description = $previousTeam
-                ? "Reassign from team '{$previousTeam}' to '{$assignedTeam->label()}'."
+            $description = $previousTeamLabel
+                ? "Reassign from '{$previousTeamLabel}' to '{$assignedTeam->label()}'."
                 : "Assigned to team '{$assignedTeam->value}'.";
 
             $this->logService->log(
@@ -115,7 +125,8 @@ class AssignmentService
                 performedBy: Auth::id(),
                 details: array_filter([
                     'assigned_team' => $assignedTeam->value,
-                    'previous_team' => $previousTeam,
+                    'previous_team' => $previousTeamLabel,
+                    'assignment_date' => Carbon::now()->format('Y-m-d H:i:s'),
                 ])
             );
 
@@ -136,13 +147,18 @@ class AssignmentService
     public function unassignUser(Model $resource): Model
     {
         if (! $resource->isAssignedToUser()) {
-            abort(422, 'resource is not assigned to any user.');
+            throw ValidationException::withMessages([
+                'user_id' => ['Resource is not assigned to any user.']
+            ]);
         }
 
         $previousAssignee = $resource->assignedUser?->name;
 
         DB::transaction(function () use ($resource, $previousAssignee) {
-            $resource->update(['assigned_to_id' => null]);
+            $resource->update([
+                'assigned_to_id' => null,
+                'assignment_date' => null,
+            ]);
 
             $this->logService->log(
                 loggable: $resource,
@@ -154,13 +170,16 @@ class AssignmentService
                 ])
             );
         });
+
         return $resource->load('assignedUser');
     }
 
     public function unassignTeam(Model $resource): Model
     {
         if (! $resource->isAssignedToTeam()) {
-            abort(422, 'resource is not assign to any team.');
+            throw ValidationException::withMessages([
+                'team' => ['Resource is not assigned to any team.']
+            ]);
         }
 
         $previousTeam = $resource->assigned_team instanceof AssignedTeam
@@ -168,7 +187,10 @@ class AssignmentService
             : $resource->assigned_team;
 
         DB::transaction(function () use ($resource, $previousTeam) {
-            $resource->update(['assigned_team' => null]);
+            $resource->update([
+                'assigned_team' => null,
+                'assignment_date' => null,
+            ]);
 
             $this->logService->log(
                 loggable: $resource,
@@ -184,30 +206,60 @@ class AssignmentService
         return $resource->load('assignedUser');
     }
 
+
+    // Private
     private function guardNotAssignable(Model $resource): void
     {
-        if (!$resource->isAssignable()) {
-            $status = $resource->status instanceof \BackedEnum
-                ? $resource->status->value
-                : $resource->status;
+        $currentStatus = $resource->status->value;
 
-            abort(422, "Resource with status '{$status}' cannot be assigned");
+        $allowedStatuses = $this->resolveAssignableStatus($resource);
+
+        if (! in_array($currentStatus, $allowedStatuses)) {
+            throw ValidationException::withMessages([
+                'status' => [
+                    "Resource with status '{$currentStatus}' cannot be assigned."
+                ]
+            ]);
         }
+    }
+
+    private function resolveAssignableStatus(Model $resource): array
+    {
+        return match (true) {
+            $resource instanceof Ticket => TicketStatus::assignableStatuses(),
+            $resource instanceof FeatureRequest => FeatureRequestStatus::assignableStatuses(),
+            $resource instanceof ErrorReport => ErrorReportStatus::assignableStatuses(),
+            default => [],
+        };
     }
 
     private function resolveStatusAfterAssignment(Model $resource): string
     {
-        $currentStatus = $resource->status instanceof \BackedEnum
+        $currentStatus = $resource->status instanceof BackedEnum
             ? $resource->status->value
             : $resource->status;
 
-        $shouldChangeToAssigned = in_array($currentStatus, [
+        $preAssignedStatus = [
             TicketStatus::Draft->value,
             TicketStatus::PendingApproval->value,
-        ]);
+            ErrorReportStatus::PendingApproval->value,
+            FeatureRequestStatus::Submission->value,
+            FeatureRequestStatus::PendingApproval->value,
+            FeatureRequestStatus::Approved->value,
+        ];
 
-        return $shouldChangeToAssigned
-            ? TicketStatus::Assigned->value
+        return in_array($currentStatus, $preAssignedStatus)
+            ? $this->resolveAssignedStatus($resource)
             : $currentStatus;
+    }
+
+    private function resolveAssignedStatus(Model $resource): string
+    {
+        return match (true) {
+            $resource instanceof Ticket => TicketStatus::Assigned->value,
+            $resource instanceof FeatureRequest => ErrorReportStatus::Assigned->value,
+            $resource instanceof ErrorReport => FeatureRequestStatus::Assigned->value,
+            default => 'assigned',
+        };
     }
 }
